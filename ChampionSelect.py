85ceb6edae82002ca274f02ee9c2d30e161fe1e3
TheAkuma010 <135660800+TheAkuma010@users.noreply.github.com>
2023-10-23 15:36:32 -0300

    Update ChampionSelect.py

--- ChampionSelect.py
+++ ChampionSelect.py
@@ -2,9 +2,9 @@ import requests
 
 nomeinvocador = input("Digite o Nome de Invocador: ")
 
-activesummoner = requests.get("https://br1.api.riotgames.com/lol/summoner/v4/summoners/by-name/" + (nomeinvocador) + "?api_key=RGAPI-82f0756c-a360-467b-89d7-cfed8f3870b9")
+activesummoner = requests.get("https://br1.api.riotgames.com/lol/summoner/v4/summoners/by-name/" + (nomeinvocador) + "?api_key=")
 summonerid = activesummoner.json()
-specdata = requests.get("https://br1.api.riotgames.com/lol/spectator/v4/active-games/by-summoner/" + (summonerid['id']) + "?api_key=RGAPI-82f0756c-a360-467b-89d7-cfed8f3870b9")
+specdata = requests.get("https://br1.api.riotgames.com/lol/spectator/v4/active-games/by-summoner/" + (summonerid['id']) + "?api_key=")
 # (activesummoner.json())
 
 print(summonerid['id'])
